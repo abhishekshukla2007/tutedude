a5c8e530a791ee9b95a04c505ee7c00977f63165
abhishekshukla2007 <abhishekshukla2007@hotmail.com>
2026-01-27 02:23:00 +0530

    Task1 and Task2 Commit

---
+++ Task1.py
@@ -0,0 +1,16 @@
+# This is Task1 file. It performs arithmatic operations on input key-in from users
+# Type conversion from String to integer before saving it to variables.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+#Perform Calculation on given input
+Addition = num1 + num2
+subtraction = num1 - num2
+Multiplication = num1 * num2
+Division = num1 / num2
+
+#Print results
+print("Addition:", Addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", Multiplication)
+print("Division:", Division)
